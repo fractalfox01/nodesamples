421635b3d8c901378a625cd77e9b040e6ab78eca
Thomas v <fractalfox01@gmail.com>
2018-04-02 12:38:02 -0700

    debugging!!!

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const ejs = require('ejs');
 const app = express();
+const path = require('path');
 
 app.set('view engine','ejs');
 
+app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', function(req, res){
-  console.log(new Date() + " : " + req.connection.remoteAddress + " : " + req.origninalUrl);
+  console.log(new Date() + " : " + req.connection.remoteAddress + " : " + req.origninalUrl + " : " + req.headers.host);
   res.render('pages/index');
 });
 
 app.get('/about', function(req, res){
-  console.log(new Date() + " : " + req.connection.remoteAddress + " : " + req.originalUrl);
+  console.log(new Date() + " : " + req.connection.remoteAddress + " : " + req.originalUrl + " : " + req.headers.host);
   res.render('pages/about');
 });
 
 app.get('*', function(req, res){
-  console.log(new Date() + " : " + req.connection.remoteAddress + " : " + req.originalUrl);
+  if(req.headers.host == "/public/styles/index.css"){
+    console.log("Asking for index.css");
+  }
+  console.log("Blah : " + req.connection.remoteAddress + " : " + req.originalUrl + " : " + req.headers.host);
+  res.end();
 });
 
 app.listen(8420, function(){
